5dd9c0c84c05a3f8f39463d7bdf3148ccb8a7f8b
langtonhugh <slangton@nscr.nl>
2024-01-09 08:48:08 +0100

    rename & delete
    delete old script for participants

--- scripts/tidytuesday_9jan2024.R
+++ scripts/tidytuesday_9jan2024.R
@@ -38,15 +38,12 @@ most_pop_df <- scrapes_df %>%
   arrange(order_var) %>% 
   slice(1)
 
-# What's your keyword?
-interest_words <- "ukraine"
+# What's your keyword(s)?
+interest_words <- "hamburg airport"
 
 # Identify a specific story.
 scrapes_df <- scrapes_df %>% 
   mutate(article_title_lower = str_to_lower(article_title),
-         interest_flag  = if_else(condition = str_detect(article_title_lower, interest_words),
-                                 true  = "yes",
-                                 false = "no"),
          interest_label = if_else(condition = str_detect(article_title_lower, interest_words),
                                  true = article_title,
                                  false = "Other"),
@@ -59,13 +56,18 @@ n_stories_vec <- length(unique(scrapes_df$interest_label))-1
 col_scheme <- c("grey95", brewer.pal(n = n_stories_vec, name = "Set2"))
 
 # Tile plot.
-ggplot(data = scrapes_df) +
-  geom_tile(mapping = aes(x = date_round, y = order_var, fill = interest_label)) +
+tile_gg <- ggplot(data = scrapes_df) +
+  geom_tile(mapping = aes(x = scrape_date, y = order_var, fill = interest_label)) +
   scale_fill_manual(values = col_scheme) +
   scale_y_reverse(breaks = c(1:10)) +
   theme_bw() +
   labs(title    = paste("BBC Top 10 'most read' involving the term:", interest_words),
        subtitle = paste("Time range between", min(scrapes_df$date_round), "and", max(scrapes_df$date_round) ),
-       fill = NULL, x = "day", y = NULL) +
-  guides(fill = guide_legend(nrow = 3)) +
+       fill = NULL, x = NULL, y = "Top 10 ranking") +
+  guides(fill = guide_legend(nrow = 4)) +
   theme(legend.position = "bottom") 
+
+# Save.
+ggsave(plot = tile_gg, filename = "visuals/interest_tile.png", width = 20, height = 12, unit = "cm")
+
+
